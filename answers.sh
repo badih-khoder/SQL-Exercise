06aeded37a04ec8b145e60b77b41cfad35bd56a0
nazim nasser <badih123khoder@gmail.com>
2021-12-12 21:17:18 +0200

    Creating-Table

--- answers.sh
+++ answers.sh
@@ -7,3 +7,32 @@ SELECT Points FROM students WHERE Name="Alex";
 INSERT INTO students VALUES(7,"badih",22,"M",500);
 UPDATE students SET Points=Points+1 WHERE name="Basma" ;
 UPDATE students SET Points=Points-1 WHERE name="Alex" ;
+
+
+
+### Creating Table
+
+CREATE TABLE graduates 
+(
+ID INTEGER PRIMARY KEY AUTOINCREMENT ,
+name TEXT NOT NULL UNIQUE,
+Age INT,
+Gender TEXT,
+Points INT,
+graduation date
+);
+
+
+###1
+INSERT INTO graduates(Name,Age,Gender,Points)
+SELECT name,Age,Gender,Points
+FROM students
+WHERE name="Layal"
+
+###2
+UPDATE graduates 
+SET Graduation = 2018-09-08 
+WHERE Name = "Layal";
+
+###3
+DELETE FROM students WHERE name="Layal";
